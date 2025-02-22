acae9515a5defbae04c6495b547f9cc810936798
JonathanFleischmann <i22006@hb.dhbw-stuttgart.degit config --global user.name>
2025-02-23 00:19:31 +0100

    Add functions to generate exception and ride exception database tables from GTFS data

---
+++ GTFSsToINSERTs/ExecuteInserts/exception_table.py
@@ -0,0 +1,49 @@
+import sys
+from ExecuteInserts.data_storage import DatabaseTable
+from ExecuteInserts.datatype_enum import DatatypeEnum
+
+
+def generate_exception_table_database_table_from_gtfs_table(calendar_dates_gtfs_table):
+    """
+    Diese Funktion 
+    """
+
+    # Finde heraus, welche Spalten in der DatabaseTabelle route vorhanden sein werden anhand der GTFSTabelle
+    gtfs_table_columns = calendar_dates_gtfs_table.get_columns()
+    database_table_columns = []
+
+
+    # Erstelle eine Liste mit den Spaltennamen der Datenbanktabelle
+    necessary_values_found = {
+        "date": False
+    }
+    used_columns = []
+    for column in gtfs_table_columns:
+        if column == "date":
+            database_table_columns.append("date_col")
+            necessary_values_found[column] = True
+            used_columns.append(column)
+    
+    # Überprüfe 
+    for necessary_value, found in necessary_values_found.items():
+        if not found:
+            print(f"Die Spalte {necessary_value} wurde nicht in der GTFS-Tabelle routes gefunden. Diese Spalte fehlt im GTFS-File", file=sys.stderr)
+            sys.exit(1)
+
+    # Erstelle ein DatabaseTable-Objekt für die Tabelle route
+    exception_table_database_table = DatabaseTable("exception_table", database_table_columns)
+
+    # Füge die Datensätze der GTFS-Tabelle route in die Datenbanktabelle ein
+    exception_table_database_table.set_all_values(
+        calendar_dates_gtfs_table.get_distinct_attributes_of_all_records(used_columns)
+    )
+
+    exception_table_database_table.add_unique_column("date_col")
+
+    exception_table_database_table.set_data_types(
+        {
+            "date_col": DatatypeEnum.DATE
+        }
+    )
+
+    return exception_table_database_table
---
+++ GTFSsToINSERTs/ExecuteInserts/ride_exception.py
@@ -0,0 +1,54 @@
+import sys
+from ExecuteInserts.data_storage import DatabaseTable
+from ExecuteInserts.datatype_enum import DatatypeEnum
+
+
+def generate_ride_exception_database_table_from_gtfs_tables(exception_table_database_table, ride_database_table, trips_gtfs_table, calendar_dates_gtfs_table):
+    """
+    Diese Funktion 
+    """
+
+    ride_exception_database_table = DatabaseTable("ride_exception", ["ride", "exception_table"])
+
+    service_date_id_service_id_map = calendar_dates_gtfs_table.get_distinct_attributes_of_all_records(["service_id"])
+    service_date_id_exception_table_id_map = exception_table_database_table.get_distinct_attributes_of_all_records(["id"])
+    trip_id_ride_id_map = ride_database_table.get_distinct_attributes_of_all_records(["id"])
+    trip_id_service_id_map = trips_gtfs_table.get_distinct_attributes_of_all_records(["service_id"])
+
+    service_id_ride_ids_map = {}
+    for trip_id, service_id in trip_id_service_id_map.items():
+        if service_id[0] not in service_id_ride_ids_map:
+            service_id_ride_ids_map[service_id[0]] = []
+        ride_id = trip_id_ride_id_map[trip_id][0]
+        if ride_id not in service_id_ride_ids_map[service_id[0]]:
+            service_id_ride_ids_map[service_id[0]].append(ride_id)
+
+    service_id_exception_table_ids_map = {}
+    for service_date_id, service_id in service_date_id_service_id_map.items():
+        if service_id[0] not in service_id_exception_table_ids_map:
+            service_id_exception_table_ids_map[service_id[0]] = []
+        exception_table_id = service_date_id_exception_table_id_map[service_date_id][0]
+        if exception_table_id not in service_id_exception_table_ids_map[service_id[0]]:
+            service_id_exception_table_ids_map[service_id[0]].append(exception_table_id)
+
+    exception_table_data = {}
+
+    for service_id, exception_table_ids in service_id_exception_table_ids_map.items():
+        for ride_id in service_id_ride_ids_map[service_id]:
+            for exception_table_id in exception_table_ids:
+                ride_exception_id = ride_id + exception_table_id
+                exception_table_data[ride_exception_id] = [ride_id, exception_table_id]
+
+    ride_exception_database_table.set_all_values(exception_table_data)
+
+    ride_exception_database_table.add_unique_column("ride")
+    ride_exception_database_table.add_unique_column("exception_table")
+
+    ride_exception_database_table.set_data_types(
+        {
+            "ride": DatatypeEnum.INTEGER,
+            "exception_table": DatatypeEnum.INTEGER
+        }
+    )
+
+    return ride_exception_database_table
